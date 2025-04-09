85aae7ae6d585a512e890016505eda18eb9ebaa9
Tytus Felbor <146979682+TFelbor@users.noreply.github.com>
2025-04-09 00:18:06 -0400

    Updates_002

--- src/api/run.py
+++ src/api/run.py
@@ -1,11 +1,10 @@
 import uvicorn
-from .main import app
 
 if __name__ == "__main__":
     uvicorn.run(
-        app,
+        "src.api.main:app",
         host="0.0.0.0",
         port=8000,
         reload=True,
-        workers=4  # Match CPU cores
+        workers=1  # Set to 1 when using reload
     )
